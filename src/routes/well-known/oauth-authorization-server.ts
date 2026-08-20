4f9a31844a17c4fe59947af284dde1c4eec9ceba
Charles Tanauan <297973909+master-kanor@users.noreply.github.com>
2026-08-21 02:14:03 +0800

    feat(oauth): RFC 8414 authorization server metadata

---
+++ src/routes/well-known/oauth-authorization-server.ts
@@ -0,0 +1,29 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { oauthMetadata } from "@/lib/oauth.server";
+
+function json(body: unknown) {
+  return new Response(JSON.stringify(body), {
+    status: 200,
+    headers: {
+      "content-type": "application/json",
+      "access-control-allow-origin": "*",
+      "cache-control": "no-store",
+    },
+  });
+}
+
+export const Route = createFileRoute("/well-known/oauth-authorization-server")({
+  server: {
+    handlers: {
+      GET: async () => json(oauthMetadata()),
+      OPTIONS: async () =>
+        new Response(null, {
+          status: 204,
+          headers: {
+            "access-control-allow-origin": "*",
+            "access-control-allow-methods": "GET, OPTIONS",
+          },
+        }),
+    },
+  },
+});
